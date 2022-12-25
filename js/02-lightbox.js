f5d39aa696fe3bee7b799464399c07ad4e3b635e
ZhukovaYu <zhukova.julia@gmail.com>
2022-12-25 14:15:16 +0100

    goit-js-hw-07
    wip task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryImg = galleryItems.map((element) => 
+  
+    `<div class="gallery__item">
+     <a class="gallery__link" href="${element.original}">
+     <img
+        class="gallery__image"
+        src="${element.preview}"
+        data-source= "${element.original}"
+        alt="${element.description}"
+     />
+     </a>
+    </div>`).join("");
+
+galleryEl.innerHTML = galleryImg;
+
+galleryEl.addEventListener("click", (event) => {
+    event.preventDefault();
+});
+
+// var lightbox = $('.gallery a').simpleLightbox({ 
+//     captionsData: 'alt',
+//     captionDelay: 250,
+//  });
+
